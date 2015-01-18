810a914af31622a3466e6b665c807dd2c5b2750d
Em01 <emma_williams1@hotmail.co.uk>
2015-01-18 20:04:05 +0000

    added some pending tests

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -2,7 +2,11 @@ require 'blackjack'
 
 describe Card do 
 
-	it "should have a value of 5" do 
-		expect(Card.new.value).to eq(5)
-	end
+	it "should have a suit"
+	it "should have a value"
+	it "should accept a suit and value when building"
+
+	# it "should have a value of 5" do 
+	# 	expect(Card.new.value).to eq(5)
+	# end
 end
