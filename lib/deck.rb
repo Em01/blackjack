a0ffd684c8e19e0615695fc7fd8f48b4b2dc478b
Em01 <emma_williams1@hotmail.co.uk>
2015-01-19 17:47:35 +0000

    working on method to build the cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,14 @@
 class Deck
+	def self.build
+		cards = []
+		[:clubs, :diamonds, :spades, :hearts].each do |suit|
+			(2..10).each do |number|
+			cards << Card.new(suit, number)
+			Card.new(suit, number)
+		end
+		cards
+	end
+	#going to occur on the class as it is a class method and will execute once
+	#The idea of a deck needs to be able to build a deck
+end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,4 +3,8 @@ require 'deck'
 
 describe Deck do 
 
+	it 'should build 52 cards' do 
+		deck = Deck.build
+		expect(deck.cards.length).to eq(52)
+	end
 end
