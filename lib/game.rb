e6146c7d982ba4f6b0af491487cacf603a15a25c
Em01 <emma_williams1@hotmail.co.uk>
2015-01-20 10:09:12 +0000

    initialize method for class Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,9 @@
 class Game
+	def initialize
+		@deck = Deck.new
+		@player_hand = Hand.new
+		@dealer_hand = Hand.new
+		2.times { @player_hand.hit!(@deck)}
+		2.times { @dealer_hand.hit!(@deck)}
+	end
 end
