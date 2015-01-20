0a316f89383b6bbc5ce1a0d861c7cc62d3438755
Em01 <emma_williams1@hotmail.co.uk>
2015-01-20 16:38:43 +0000

    looking at deck mock

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,11 +1,14 @@
 require 'hand'
 
+
 describe Hand do 
 
 	it "should calculate the value correctly" do 
-		mock_cards = [Card.new(:clubs, 5), Card.new(:diamonds, 10)]
+		deck = mock(:deck, :cards => [Card.new(:clubs, 5), Card.new(:diamonds, 10)]) 
+		#mock deck-has to respond to cards
 		hand = Hand.new
-		hand.stub(:cards) { mock_cards } #stub out cards and have it return cards
+		2.times { hand.hit!(deck) } #rather than a real deck it will call the above
+		# hand.stub(:cards) { mock_cards } #stub out cards and have it return cards
 		expect(hand.value).to eq (15)
 	end
 end
