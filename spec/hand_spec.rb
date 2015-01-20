60431c9bdf182f29f185329c089406dde064312b
Em01 <emma_williams1@hotmail.co.uk>
2015-01-20 17:31:17 +0000

    amended issue with mock, renamed to double and test coverage is now good

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -4,11 +4,9 @@ require 'hand'
 describe Hand do 
 
 	it "should calculate the value correctly" do 
-		deck = mock(:deck, :cards => [Card.new(:clubs, 5), Card.new(:diamonds, 10)]) 
-		#mock deck-has to respond to cards
+		deck = double(:deck, :cards => [Card.new(:clubs, 5), Card.new(:diamonds, 10)]) 
 		hand = Hand.new
-		2.times { hand.hit!(deck) } #rather than a real deck it will call the above
-		# hand.stub(:cards) { mock_cards } #stub out cards and have it return cards
+		2.times { hand.hit!(deck) }
 		expect(hand.value).to eq (15)
 	end
 end
